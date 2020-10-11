a6bba785ddbe2515c04e7521a0fa152fa2f52e32
Kiril.Simonovich <ska_27_blr@mail.ru>
2020-10-11 21:56:33 +0300

    add CreateDeviceTest

---
+++ src/main/java/models/api/SearchCriteria.java
@@ -0,0 +1,32 @@
+
+package models.api;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+
+@Generated("net.hexar.json2pojo")
+@SuppressWarnings("unused")
+public class SearchCriteria {
+
+    @Expose
+    private String searchOption;
+    @Expose
+    private String searchTerms;
+
+    public String getSearchOption() {
+        return searchOption;
+    }
+
+    public void setSearchOption(String searchOption) {
+        this.searchOption = searchOption;
+    }
+
+    public String getSearchTerms() {
+        return searchTerms;
+    }
+
+    public void setSearchTerms(String searchTerms) {
+        this.searchTerms = searchTerms;
+    }
+
+}
