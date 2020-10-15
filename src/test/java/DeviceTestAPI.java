c91a4ae035cb78730a1d9f17f6a3af0680590631
Kiril.Simonovich <ska_27_blr@mail.ru>
2020-10-15 14:53:55 +0300

    refactor style

--- src/test/java/DeviceTestAPI.java
+++ src/test/java/DeviceTestAPI.java
@@ -10,7 +10,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import services.DeviceService;
 
-
 public class DeviceTestAPI {
 
     DeviceService deviceService = new DeviceService();
@@ -18,8 +17,7 @@ public class DeviceTestAPI {
     @Test(dataProvider = "getRequestBodyToCreateDevice", dataProviderClass = CustomerDataprovider.class)
     public void createDeviceTest(RequestDeviceCreate requestBody) {
         ResponseDeviceCreate createdDevice = deviceService.createDevicePositiv(requestBody);
-        Assert.assertEquals(createdDevice.getResult().getMessage()
-                , "New NetworkDevice has been created with Serial Number: "
+        Assert.assertEquals(createdDevice.getResult().getMessage(), "New NetworkDevice has been created with Serial Number: "
                         + createdDevice.getResult().getDevice().getSerialNumber(), "Device is not created");
         deviceService.deleteDevicePositiv(createdDevice.getResult().getDevice().getSerialNumber());
     }
@@ -49,8 +47,7 @@ public class DeviceTestAPI {
         requestBody.setName(null);
         ResponseDeviceCreate createdDevice = deviceService.createDeviceNegativ(requestBody);
         Assert.assertEquals(createdDevice.getErrorCode(), -100.0, "Error code is not -100");
-        Assert.assertEquals(createdDevice.getErrorMessage()
-                , "Unable to create network device: Please make sure that all the mandatory properties have been provided! ( type, name, address, locationName)"
+        Assert.assertEquals(createdDevice.getErrorMessage(), "Unable to create network device: Please make sure that all the mandatory properties have been provided! ( type, name, address, locationName)"
                 , "Unexpected error message, Error message: " + createdDevice.getErrorMessage());
         Assert.assertNull(createdDevice.getResult().getDevice(), "There is a device that should not be");
         Assert.assertNull(createdDevice.getResult().getMessage(), "Unexpected message, Error message: " + createdDevice.getErrorMessage());
@@ -66,9 +63,8 @@ public class DeviceTestAPI {
     public void deleteDifferentDevice(RequestDeviceCreate requestBody) {
         ResponseDeviceCreate createdDevice = deviceService.createDevicePositiv(requestBody);
         ResponseDeviceDelete responseDeleteDevice = deviceService.deleteDevicePositiv(createdDevice.getResult().getDevice().getSerialNumber());
-        Assert.assertEquals(responseDeleteDevice.getResult().getMessage()
-                , "Successful! Total 1 devices have been deleted. "
-                , "Unexpected message, Error message: " + responseDeleteDevice.getErrorMessage());
+        Assert.assertEquals(responseDeleteDevice.getResult().getMessage(),"Successful! Total 1 devices have been deleted. "
+                ,"Unexpected message, Error message: " + responseDeleteDevice.getErrorMessage());
     }
 
     @Test
@@ -84,20 +80,18 @@ public class DeviceTestAPI {
         deviceService.deleteDevicePositiv(responseDeviceCreate.getResult().getDevice().getSerialNumber());
         ResponseDeviceDelete responseDeleteDevice = deviceService.deleteDeviceNegativ(responseDeviceCreate.getResult().getDevice().getSerialNumber());
         Assert.assertEquals(responseDeleteDevice.getErrorCode(), -4.0, "Error code is not -1, Error code: " + responseDeleteDevice.getErrorCode());
-        Assert.assertEquals(responseDeleteDevice.getErrorMessage()
-                , "No devices matched the search criterias"
-                , "Unexpected error message, Error message: " + responseDeleteDevice.getErrorMessage());
+        Assert.assertEquals(responseDeleteDevice.getErrorMessage(),"No devices matched the search criterias"
+                ,"Unexpected error message, Error message: " + responseDeleteDevice.getErrorMessage());
         Assert.assertEquals(responseDeleteDevice.getResult().getMessage(), "Failed"
-                , "Unexpected message, Error message: " + responseDeleteDevice.getErrorMessage());
+                ,"Unexpected message, Error message: " + responseDeleteDevice.getErrorMessage());
     }
 
     @Test(dataProvider = "getDataToUpdateDevice", dataProviderClass = CustomerDataprovider.class)
     public void updateDevice(RequestDeviceCreate requestBody, RequestDeviceUpdate updateDevice) {
         ResponseDeviceCreate createdDevice = deviceService.createDevicePositiv(requestBody);
         ResponseDeviceUpdate responseUpdateDevice = deviceService.updateDevicePositiv(updateDevice);
-        Assert.assertEquals(responseUpdateDevice.getResult().getMessage()
-                , "Updated successfully"
-                , "Unexpected message, Error message: " + responseUpdateDevice.getErrorMessage());
+        Assert.assertEquals(responseUpdateDevice.getResult().getMessage(), "Updated successfully"
+                ,"Unexpected message, Error message: " + responseUpdateDevice.getErrorMessage());
         deviceService.deleteDevicePositiv(createdDevice.getResult().getDevice().getSerialNumber());
     }
 
@@ -105,9 +99,8 @@ public class DeviceTestAPI {
     public void updateDeviceAddCommentTest(RequestDeviceCreate requestBody, RequestDeviceUpdate updateDevice) {
         ResponseDeviceCreate createdDevice = deviceService.createDevicePositiv(requestBody);
         ResponseDeviceUpdate responseUpdateDevice = deviceService.updateDevicePositiv(updateDevice);
-        Assert.assertEquals(responseUpdateDevice.getResult().getMessage()
-                , "Updated successfully"
-                , "Unexpected message, Error message: " + responseUpdateDevice.getErrorMessage());
+        Assert.assertEquals(responseUpdateDevice.getResult().getMessage(),"Updated successfully"
+                ,"Unexpected message, Error message: " + responseUpdateDevice.getErrorMessage());
         deviceService.deleteDevicePositiv(createdDevice.getResult().getDevice().getSerialNumber());
     }
 
@@ -119,9 +112,8 @@ public class DeviceTestAPI {
         ResponseDeviceUpdate responseUpdateDevice = deviceService.updateDeviceNegativ(updateDevice);
         Assert.assertEquals(responseUpdateDevice.getErrorCode(), -412.0, "Error code is not -412, Error code: " + responseUpdateDevice.getErrorCode());
         Assert.assertEquals(responseUpdateDevice.getErrorMessage(), "Specify at least one parameter to be modified", "Unexpected error message, Error message: " + responseUpdateDevice.getErrorMessage());
-        Assert.assertEquals(responseUpdateDevice.getResult().getMessage()
-                , "Failed"
-                , "Unexpected message, Error message: " + responseUpdateDevice.getErrorMessage());
+        Assert.assertEquals(responseUpdateDevice.getResult().getMessage(),"Failed"
+                ,"Unexpected message, Error message: " + responseUpdateDevice.getErrorMessage());
         deviceService.deleteDevicePositiv(createdDevice.getResult().getDevice().getSerialNumber());
     }
 
@@ -130,18 +122,16 @@ public class DeviceTestAPI {
         updateDevice.setSerialNumber(null);
         ResponseDeviceCreate createdDevice = deviceService.createDevicePositiv(requestBody);
         ResponseDeviceUpdate responseUpdateDevice = deviceService.updateDevicePositiv(updateDevice);
-        Assert.assertEquals(responseUpdateDevice.getResult().getMessage()
-                , "Updated successfully"
-                , "Unexpected message, Error message: " + responseUpdateDevice.getErrorMessage());
+        Assert.assertEquals(responseUpdateDevice.getResult().getMessage(),"Updated successfully"
+                ,"Unexpected message, Error message: " + responseUpdateDevice.getErrorMessage());
         deviceService.deleteDevicePositiv(createdDevice.getResult().getDevice().getSerialNumber());
     }
 
     @Test(dataProvider = "getDataToMoveDevice", dataProviderClass = CustomerDataprovider.class)
     public void moveDeviceTest(ResponseDeviceCreate responseDeviceCreate, RequestDeviceMove moveDevice) {
         ResponseDeviceMove responseMoveDevice = deviceService.moveDevicePositiv(moveDevice);
-        Assert.assertEquals(responseMoveDevice.getResult().getMessage()
-                , "Total 0 device(s) has(have) been moved to department with name: Core Infrastructure. Total 1 device(s) has(have) been ignored due to same department violation."
-                , "Unexpected message, Error message: " + responseMoveDevice.getErrorMessage());
+        Assert.assertEquals(responseMoveDevice.getResult().getMessage(),"Total 0 device(s) has(have) been moved to department with name: Core Infrastructure. Total 1 device(s) has(have) been ignored due to same department violation."
+                ,"Unexpected message, Error message: " + responseMoveDevice.getErrorMessage());
         deviceService.deleteDevicePositiv(responseDeviceCreate.getResult().getDevice().getSerialNumber());
     }
 
@@ -149,12 +139,10 @@ public class DeviceTestAPI {
     public void moveDeviceWithoutSearchCriteriaTest(ResponseDeviceCreate responseDeviceCreate, RequestDeviceMove moveDevice) {
         moveDevice.setSearchCriterias(null);
         ResponseDeviceMove responseMoveDevice = deviceService.moveDeviceNegativ(moveDevice);
-        Assert.assertEquals((responseMoveDevice.getErrorCode())
-                , (-412.0)
-                , "Error code is not -412, Error code: " + responseMoveDevice.getErrorCode());
-        Assert.assertEquals(responseMoveDevice.getErrorMessage()
-                , "Search criterias can not be null."
-                , "Unexpected error message, Error message: " + responseMoveDevice.getErrorMessage());
+        Assert.assertEquals((responseMoveDevice.getErrorCode()),(-412.0)
+                ,"Error code is not -412, Error code: " + responseMoveDevice.getErrorCode());
+        Assert.assertEquals(responseMoveDevice.getErrorMessage(),"Search criterias can not be null."
+                ,"Unexpected error message, Error message: " + responseMoveDevice.getErrorMessage());
         Assert.assertNull(responseMoveDevice.getResult().getMessage(), "There is a message that should not be");
         deviceService.deleteDevicePositiv(responseDeviceCreate.getResult().getDevice().getSerialNumber());
     }
@@ -163,12 +151,10 @@ public class DeviceTestAPI {
     public void moveDeviceWithoutDepartmentTest(ResponseDeviceCreate responseDeviceCreate, RequestDeviceMove moveDevice) {
         moveDevice.setAccountSerialNumber(null);
         ResponseDeviceMove responseMoveDevice = deviceService.moveDeviceNegativ(moveDevice);
-        Assert.assertEquals(responseMoveDevice.getErrorCode()
-                , -412.0
-                , "Error code is not -412, Error code: " + responseMoveDevice.getErrorCode());
-        Assert.assertEquals(responseMoveDevice.getErrorMessage()
-                , "Destination account serial number is mandatory to be provided."
-                , "Unexpected error message, Error message: " + responseMoveDevice.getErrorMessage());
+        Assert.assertEquals(responseMoveDevice.getErrorCode(), -412.0
+                ,"Error code is not -412, Error code: " + responseMoveDevice.getErrorCode());
+        Assert.assertEquals(responseMoveDevice.getErrorMessage(),"Destination account serial number is mandatory to be provided."
+                ,"Unexpected error message, Error message: " + responseMoveDevice.getErrorMessage());
         Assert.assertNull(responseMoveDevice.getResult().getMessage(), "There is a message that should not be");
         deviceService.deleteDevicePositiv(responseDeviceCreate.getResult().getDevice().getSerialNumber());
     }
