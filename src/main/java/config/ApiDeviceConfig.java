1452a4af712c6dff65ee4702bef6418bc93cfdd0
Kiril.Simonovich <ska_27_blr@mail.ru>
2020-10-20 11:43:14 +0300

    add enum ApiDeviceConfig

---
+++ src/main/java/config/ApiDeviceConfig.java
@@ -0,0 +1,23 @@
+package config;
+
+/**
+ * Enum class contains constant path for use when forming request.
+ */
+public enum ApiDeviceConfig {
+
+    CREATE_DEVICE("/admin/device/create"),
+    DELETE_DEVICE("/admin/device/delete"),
+    UPDATE_DEVICE("/admin/device/update"),
+    MOVE_DEVICE("/admin/device/move");
+
+    String path;
+
+    ApiDeviceConfig(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
+
