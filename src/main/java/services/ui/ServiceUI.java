9667887406438af72403717ac002f0db84f185db
Kiril.Simonovich <ska_27_blr@mail.ru>
2020-10-26 10:40:34 +0300

    refactor Page Object pattern

--- src/main/java/services/ui/ServiceUI.java
+++ src/main/java/services/ui/ServiceUI.java
@@ -11,7 +11,11 @@ public class ServiceUI {
         return authenficationPage.submitCreateNewAccount();
     }
 
-    public MyAccountPage fillPersonalInformation(CreateAccountPage accountPage, boolean isMister, String customer_firstname, String customer_lastname, String password, String day, String month, String year, boolean checkbox, boolean optin, String firstname, String lastname, String company, String address1, String address2, String city, String state, String postcode, String other, String phone, String phone_mobile, String alias) {
+    public MyAccountPage fillPersonalInformation(CreateAccountPage accountPage, boolean isMister,
+           String customer_firstname, String customer_lastname, String password, String day, String month, String year,
+           boolean checkbox, boolean optin, String firstname, String lastname, String company, String address1,
+           String address2, String city, String state, String postcode, String other, String phone, String phone_mobile,
+           String alias) {
         accountPage.isMister(isMister);
         accountPage.enterCustomerFirstName(customer_firstname);
         accountPage.enterCustomerLastName(customer_lastname);
