a0137a76fcfa970be5c421e46487b054956f4d78
Kiril.Simonovich <ska_27_blr@mail.ru>
2020-11-03 12:31:33 +0300

    fix framework

--- src/main/java/ui/providers/DriverProvider.java
+++ src/main/java/ui/providers/DriverProvider.java
@@ -10,21 +10,14 @@ import java.util.concurrent.TimeUnit;
  */
 public final class DriverProvider {
 
-    private static volatile DriverProvider provider;
-
     private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
-
-    private DriverProvider(WebDriver driver) {
-        System.setProperty("webdriver.chrome.driver", "C:\\work\\Drivers\\SeleniumWebDriver\\chromedriver.exe");
-        DRIVER.set(driver);
-        DRIVER.set(new ChromeDriver());
-        DRIVER.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        DRIVER.get().manage().window().maximize();
-    }
 
     public static synchronized WebDriver getDriver() {
         if (DRIVER.get() == null) {
-            provider = new DriverProvider(DRIVER.get());
+            System.setProperty("webdriver.chrome.driver", "C:\\work\\Drivers\\SeleniumWebDriver\\chromedriver.exe");
+            DRIVER.set(new ChromeDriver());
+            DRIVER.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            DRIVER.get().manage().window().maximize();
         }
         return DRIVER.get();
     }
