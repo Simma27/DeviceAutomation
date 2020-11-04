1a69f2a14b173394dcdb4cf3adb97491c93ba819
Kiril.Simonovich <ska_27_blr@mail.ru>
2020-11-04 15:03:41 +0300

    change pom

--- src/main/java/ui/providers/DriverProvider.java
+++ src/main/java/ui/providers/DriverProvider.java
@@ -1,8 +1,11 @@
 package ui.providers;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,35 +16,35 @@ public final class DriverProvider {
 
     private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
 
-//    public static synchronized WebDriver getDriver() {
-//        if (DRIVER.get() == null) {
-//            RemoteWebDriver remoteWebDriver;
-//            final DesiredCapabilities capabilities = new DesiredCapabilities();
-//            capabilities.setCapability("browserName", "chrome");
-//            capabilities.setCapability("browserVersion", "86.0");
-//            capabilities.setCapability("enableVNC", true);
-//            capabilities.setCapability("screenResolution", "1920x1080x24");
-//            try {
-//                remoteWebDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-//                remoteWebDriver.manage().window().maximize();
-//                remoteWebDriver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
-//                DRIVER.set(remoteWebDriver);
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return DRIVER.get();
-//    }
-
     public static synchronized WebDriver getDriver() {
         if (DRIVER.get() == null) {
-            System.setProperty("webdriver.chrome.driver", "C:\\work\\Drivers\\SeleniumWebDriver\\chromedriver.exe");
-            DRIVER.set(new ChromeDriver());
-            DRIVER.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            DRIVER.get().manage().window().maximize();
+            RemoteWebDriver remoteWebDriver;
+            final DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("browserName", "chrome");
+            capabilities.setCapability("browserVersion", "86.0");
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("screenResolution", "1920x1080x24");
+            try {
+                remoteWebDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+                remoteWebDriver.manage().window().maximize();
+                remoteWebDriver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+                DRIVER.set(remoteWebDriver);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         return DRIVER.get();
     }
+
+//    public static synchronized WebDriver getDriver() {
+//        if (DRIVER.get() == null) {
+//            System.setProperty("webdriver.chrome.driver", "C:\\work\\Drivers\\SeleniumWebDriver\\chromedriver.exe");
+//            DRIVER.set(new ChromeDriver());
+//            DRIVER.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//            DRIVER.get().manage().window().maximize();
+//        }
+//        return DRIVER.get();
+//    }
 
     public static void tearDown() {
         if (DRIVER.get() != null) {
